3fffa1c0b8a4042a294de52265ec4cc814e626ef
CN_Zhou <37177060+ChinaZYZ@users.noreply.github.com>
2022-08-27 15:00:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: '盗铃', // 作者
   BIO: '一个乐观的颓废青年⭐', // 作者简介
-  LINK: 'www.mucoder.top/about', // 网站地址
+  LINK: 'about', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:process.env.NOTION_PAGE_ID || 'ae07517b44f8466ca2fbc7048e15ecc6', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
